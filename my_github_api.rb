ac7791350e64c9ba33e24d632db56d07b76d3570
Conrad Petrich <conradpetrich@gmail.com>
2016-09-24 23:34:54 -0600

    updated my_github_api file to be more generic and removed my own info.

--- my_github_api.rb
+++ my_github_api.rb
@@ -3,32 +3,6 @@ require 'json'
 require 'net/smtp'
 require 'aws-sdk'
 
-
-# NOTES:
-
-# In order to make this work, you must ensure you have a current version of ruby and the proper gems installed
-# some of which are required above. The gems needed to run this scrip can be installed via command line:
-
-# gem install json aws-sdk
-
-# The rubygems and net/smtp gems are built into ruby and shouldn't need to be installed as long 
-# as you have a current version of ruby.
-
-# Assumptions =>
-# 1- user has valid github credentials
-# 2- user is a member of at leaset one github organization
-# 3- members of organization have public emails
-# 4- user has access to an AWS S3 Bucket
-# 5- user has already created a file storing the users AWS user credentials in ~/.aws/credentails file like so:
-#
-# [name_of_credentials]
-# aws_access_key_id = your_key_id
-# aws_secret_access_key = your_access_key
-#
-# 6- bucket is within region:'us-east-1'
-# 7- user knows the smtp server's info in order to send emails correctly
-
-
 # prompt user for Github username then desired organization
 puts "enter user name"
 user_name = gets.chomp.to_s
@@ -75,7 +49,7 @@ unless members_without_names.empty?
     # connect to bucket in speicified region
     # I am assuming that the region is us-east-1 which is also for US Standard.
     s3 = Aws::S3::Resource.new(region:'us-east-1')
-    obj = s3.bucket("#{bucket_name}").object('GithubUsersWithoutNames')
+    obj = s3.bucket("#{bucket_name}").object('github_nonames')
 
     # save text directly to file with .put method for s3 bucket
     obj.put(body: "#{user_names.join(', ')}")
@@ -91,14 +65,21 @@ unless members_without_names.empty?
   # The subject and body of the email are seperated by two new line characters
   message = "Subject: Hey There!\n\nBe sure to enter your name in you Github account by following the link below.\nhttps://github.com/settings/profile?profile_link=1"
 
+
   begin
-    # smtp = Net::SMTP.new('smtp_server_name', port_number)
+    ######################### Replace ###########################
+    # Currently set to work with Gmail, and if you don't want to use gmail you will need to replace information:
+
+    # 1- replace smtp.gmail.com with another smtp server name (or leave as is if using gmail)
+    # 2- replace 587 with another port number
     smtp = Net::SMTP.new('smtp.gmail.com', 587)
     smtp.enable_starttls
 
-#### Replace 
-    # smtp.start('domain', 'username', 'password', :login)
+    # 1- replace mail.google.com with domain of mail server (or leave as is if using gmail)
+    # 2- replace johndoe with username
+    # 3- replace secret with password 
     smtp.start('mail.google.com', 'johndoe', 'secret', :login) do
+      # 1- replace johndoe@gmail.com with the email with the credentials on line above
       smtp.send_message(message, "johndoe@gmail.com", recipient_emails)
     end
   rescue Exception => e
@@ -106,6 +87,7 @@ unless members_without_names.empty?
     puts e.message
     puts e.backtrace
   end
+
 else
   puts "There are not members without names within the organization: #{organization_name}"
 end
